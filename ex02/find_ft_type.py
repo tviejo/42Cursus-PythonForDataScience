0d47bf23b2eea6388432924f985ed421115430fa
Thomas Viejo <tviejo@MADE-f0Br6s2.clusters.42paris.fr>
2025-01-09 17:51:04 +0100

    ex02

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,15 @@
+def all_thing_is_obj(object: any) -> int:
+    if object.__class__ == list:
+        print("List : ", object.__class__)
+    elif object.__class__ == tuple:
+        print("Tuple : ", object.__class__)
+    elif object.__class__ == set:
+        print("Set : ", object.__class__)
+    elif object.__class__ == dict:
+        print("Dict : ", object.__class__) 
+    elif object.__class__ == str:
+        print(object, "is in the kitchen :", object.__class__)
+    else:
+        print("Type not found")
+        return(42)
+    
