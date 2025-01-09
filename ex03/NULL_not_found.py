3dd706a6f13e4ba1638c2b10185c941976f3a532
Thomas Viejo <tviejo@MADE-f0Br6s2.clusters.42paris.fr>
2025-01-09 19:03:59 +0100

    ex03 started

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def NULL_not_found(object: any) -> int:
+    if object.__class__ == list:
+        print("List : ", object.__class__)
+    elif object.__class__ == tuple:
+        print("Tuple : ", object.__class__)
+    elif object.__class__ == set:
+        print("Set : ", object.__class__)
+    elif object.__class__ == dict:
+        print("Dict : ", object.__class__) 
+    elif object.__class__ == str:
+        print(object, "is in the kitchen :", object.__class__)
+    else:
+        print("Type not found")
+        return(42)
+    
