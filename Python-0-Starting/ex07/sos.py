9105e26e66eb32e4f0854839907266cc6f6a58ba
tviejo <tviejo@student.42.fr>
2025-01-11 17:51:58 +0100

    ex07 done

---
+++ Python-0-Starting/ex07/sos.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def main():
+    """
+    Entry point of the program.
+    """
+    if len(sys.argv) != 2:
+        print("AssertionError: the arguments are bad")
+        sys.exit(1)
+
+    string = sys.argv[1]
+    for char in string:
+        if not char.isalnum() and char != " ":
+            print("AssertionError: the arguments are bad")
+            sys.exit(1)
+
+    NESTED_MORSE = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+        '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+        '7': '--...', '8': '---..', '9': '----.', ' ': '/'
+    }
+
+    morse_code = ""
+    for index, char in enumerate(string):
+        if index != 0:
+            morse_code += " "
+        morse_code += NESTED_MORSE[char.upper()]
+    print(morse_code)
+
+
+if __name__ == "__main__":
+    main()
