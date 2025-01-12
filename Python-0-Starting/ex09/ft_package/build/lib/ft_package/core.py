c282e3595e892434156806a8e152e2bc8bfdaddf
tviejo <tviejo@student.42.fr>
2025-01-12 12:33:33 +0100

    ex09 done

---
+++ Python-0-Starting/ex09/ft_package/build/lib/ft_package/core.py
@@ -0,0 +1,12 @@
+def count_in_list(elements, target):
+    """
+    Counts occurrences of `target` in the list `elements`.
+
+    Args:
+        elements (list): List of elements to search.
+        target (any): Element to count.
+
+    Returns:
+        int: Count of occurrences of `target`.
+    """
+    return elements.count(target)
---
+++ Python-0-Starting/ex09/test.py
@@ -0,0 +1,4 @@
+from ft_package import count_in_list
+
+print(count_in_list(["toto", "tata", "toto"], "toto"))  # Output: 2
+print(count_in_list(["toto", "tata", "toto"], "tutu"))  # Output: 0
