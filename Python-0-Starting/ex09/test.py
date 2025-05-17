ea7b9037e9a0d47ab9ac79e55841927758cdaac1
Thomas Viejo <tviejo12@gmail.com>
2025-05-17 18:45:55 +0200

    improved test

--- Python-0-Starting/ex09/test.py
+++ Python-0-Starting/ex09/test.py
@@ -2,3 +2,4 @@ from ft_package import count_in_list
 
 print(count_in_list(["toto", "tata", "toto"], "toto"))  # Output: 2
 print(count_in_list(["toto", "tata", "toto"], "tutu"))  # Output: 0
+print(count_in_list(["toto", "tata", "toto"], "tata"))  # Output: 1
